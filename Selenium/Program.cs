eb2b0475813448977366d35d15c8f66c9e2ddd23
Bruizz66 <brayanruizpalacio77@gmail.com>
2021-10-19 16:16:13 -0500

    Primer proyecto de consola con Nunit test C#

---
+++ Selenium/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Selenium
+{
+    class Program
+    {
+        IWebDriver driver = new ChromeDriver();
+        static void Main(string[] args)
+        {
+        }
+
+        [SetUp]
+        public void Initialize()
+        {
+            driver.Navigate().GoToUrl("https://www.google.com.co/");
+            Console.WriteLine("Driver initialized");
+        }
+
+        [Test]
+        public void ExecuteTest()
+        {
+            IWebElement barraNav = driver.FindElement(By.Name("q"));
+            barraNav.SendKeys("Hola mundo");
+            Console.WriteLine("Test executed");
+        }
+
+        [TearDown]
+        public void CleanUp()
+        {
+            driver.Close();
+            Console.WriteLine("Driver Closed");
+        }
+    }
+}
